ea4c7ec2ad18fea4b9a4eef1d002e623acb0ca8c
João Victor <47671724+jovctr1899@users.noreply.github.com>
2019-09-11 15:28:11 -0300

    ParticipaProjeto

---
+++ SGEP/Models/ParticipaProjeto.cs
@@ -0,0 +1,14 @@
+﻿using SGEP.Models.Validacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGEP.Models
+{
+    public class ParticipaProjeto : IAutoValida
+    {
+        public ulong idFuncionario { get; set; }
+        public ulong idProjeto { get; set; }
+    }
+}
