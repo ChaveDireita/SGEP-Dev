4f8f330d71ae4a28b7f9e3849ed243b4f15e46aa
Paulo Sérgio <47671822+psergio161@users.noreply.github.com>
2019-09-15 20:21:07 -0300

    Adicionando ICollection

--- SGEP/Models/Material.cs
+++ SGEP/Models/Material.cs
@@ -9,28 +9,30 @@ namespace SGEP.Models
     {
         [Key]
         public ulong Id { get; set; }
-        [Range(0, double.PositiveInfinity, ErrorMessage = "A quantidade n„o pode ser menor que 0.")]
+        [Range(0, double.PositiveInfinity, ErrorMessage = "A quantidade n√£o pode ser menor que 0.")]
         public decimal Quantidade { get; set; } = 0;
-        public string Nome { get; set; }
+        public string Descricao { get; set; }
         public string Unidade { get; set; }
 
+        public virtual ICollection<Projeto> Projetos { get; set; }
+
         private decimal _preco;
-        [Range(0, double.PositiveInfinity, ErrorMessage = "O preÁo n„o pode ser menor que 0.")]
+        [Range(0, double.PositiveInfinity, ErrorMessage = "O pre√ßo n√£o pode ser menor que 0.")]
         [Column(TypeName = "decimal(27, 2)")]
-        public decimal Preco
+        public decimal PrecoUnitario
         {
-          get
-          {
-             return _preco;
-          }
-          set
-          {
-            if (value > 0)
-              _preco = value;
-          }
+            get
+            {
+                return _preco;
+            }
+            set
+            {
+                if (value > 0)
+                    _preco = value;
+            }
         }
 
-        public bool Validar() => Preco >= 0 && Quantidade >= 0 && !string.IsNullOrEmpty(Nome) && !string.IsNullOrEmpty(Unidade);
+        public bool Validar() => PrecoUnitario >= 0 && Quantidade >= 0 && !string.IsNullOrEmpty(Descricao) && !string.IsNullOrEmpty(Unidade);
 
         //public void RemoverMaterial(double quantidade)
         //{
