db44cd3fb4abc7c72aa3092f886afd6454e73aa1
psergio <paulo.sergiopnhr161@hotmail.com>
2019-11-06 20:54:19 -0300

    View Model Funcionario

--- SGEP-Site/Models/Funcionario/FuncionarioCreateViewModel.cs
+++ SGEP-Site/Models/Funcionario/FuncionarioCreateViewModel.cs
@@ -8,6 +8,10 @@ namespace SGEP_Site.Models
 {
     public class FuncionarioCreateViewModel
     {
+        [RegularExpression("[0-9]+", ErrorMessage = "Apenas números são permitidos nesse campo \"Id.\"")]
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        [Display(Name = "Código")]
+        public string Id { get; set; }
         [RegularExpression("[A-z ÁÁÄáàäÉÈËéèëÍÌÏíìïÓÒÖóòöÚÙÜúùü]+", ErrorMessage = "Apenas letras são permitidas no campo \"Nome completo.\"")]
         [Required(ErrorMessage = "Este campo é obrigatório.")]
         [Display(Name = "Nome completo")]
